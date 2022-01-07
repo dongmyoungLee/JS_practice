cc3b9d8dc91efe0b82dbfeb944b7a87c169e9271
pajang1515 <73331335+dongmyoungLee@users.noreply.github.com>
2022-01-07 17:06:30 +0900

    slide_Symbol

--- JS_ES6(2)/slide_class/slide4.js
+++ JS_ES6(2)/slide_class/slide4.js
@@ -1,4 +1,6 @@
 // 외부 동작에 의해서도 슬라이드 전환이 이루어졌으면 좋겠다는 요구사항에 대한 대응 -> 차라리 prototype 활용하여 이런식으로 개편
+// 메소드를 각자 다 들고 다닐 필요는 없는건 좋지만 각 프로퍼티에 대한 정보가 외부로 유출되는 단점이 있음
+// 또한 slide 요소를 외부에서 접근할 수 있음. 접근 하는 순간 무언가 방어 해주는 방어 로직이 필요함
 
 function Slide($target, slideData) {
     const frag = document.createDocumentFragment()
---
+++ JS_ES6(2)/slide_class/slide5.js
@@ -0,0 +1,50 @@
+
+// 안전성을 위해 private 멤버 활용 (Symbol)
+// key 값이 Symbol 이기 때문에 접근할 수 있는 방법이 마땅치않음
+
+const Slide = (() => {
+    const CONTAINER = Symbol('container')
+    const SLIDE_DATA = Symbol('SlideData')
+    const CURRENT_INDEX = Symbol('currentIndex')
+
+    return class {
+        constructor($target, slideData) {
+            const frag = document.createDocumentFragment()
+            const $slide = document.createElement('div')
+
+            this[CONTAINER] = document.createElement('ul')
+            this[CURRENT_INDEX] = 0
+            this[SLIDE_DATA] = slideData
+
+            $slide.className = 'slide'
+            this[CONTAINER].className = 'slide__container'
+
+            this[CONTAINER].style.width = slideData.length * 100 + 'px'
+            slideData.forEach((v, i) => {
+                const $li = document.createElement('li')
+                $li.className = 'slide__item'
+                $li.innerText = v
+                this[CONTAINER].appendChild($li)
+            })
+            $slide.appendChild(this[CONTAINER])
+            $slide.addEventListener('click', this.triggerClick.bind(this))
+
+            frag.appendChild($slide)
+            $target.appendChild(frag)
+        }
+
+        triggerClick(e) {
+            e && e.preventDefault()
+            this[CURRENT_INDEX] = (this[CURRENT_INDEX] + 1) % this[SLIDE_DATA].length
+            this[CONTAINER].style.left = -100 * this[CURRENT_INDEX] + 'px'
+        }
+    }
+})()
+
+document.body.innerHTML += '<div id="a"></div><div id="b"></div><div id="c"></div><div id="d"></div><div id="e"></div>'
+
+const slide1 = new Slide(document.getElementById('a'), [1,2,3,4,5])
+const slide2 = new Slide(document.getElementById('b'), ['a', 'b', 'c', 'd', 'e'])
+const slide3 = new Slide(document.getElementById('c'), ['A', 'B', 'C', 'D', 'E'])
+
+console.log(slide1)
