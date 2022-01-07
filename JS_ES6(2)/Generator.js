742586c6799123dea574898c377c846806e071f6
pajang1515 <73331335+dongmyoungLee@users.noreply.github.com>
2022-01-07 14:47:06 +0900

    22/01/07

--- JS_ES6(2)/Generator.js
+++ JS_ES6(2)/Generator.js
@@ -57,3 +57,112 @@ const obj1 = {
 
 console.log(...obj1)
 
+function* gene2() {
+    yield* [1,2,3,4,5]
+    yield
+    yield* 'abcde'
+}
+
+const gen1 = gene2()
+console.log(gen1.next())
+console.log(gen1.next())
+console.log(gen1.next())
+console.log(gen1.next())
+console.log(gen1.next())
+console.log(gen1.next())
+console.log(gen1.next())
+console.log(gen1.next())
+console.log(gen1.next())
+console.log(gen1.next())
+console.log(gen1.next())
+console.log(gen1.next())
+
+function* geneT() {
+    let first = yield 1
+    console.log(first)
+    let second = yield first + 2
+    console.log(second)
+    yield second + 3
+}
+
+const genT = geneT()
+
+console.log(genT.next())
+console.log(genT.next(10))
+console.log(genT.next(20))
+
+/*
+
+비동기 처리 예시
+
+#원하는 작업
+
+userId 가 1000번 이후인 데이터를 가져와서
+그 중에 4번째 위치한 User 정보를 보고싶음
+
+*/
+
+// const ajaxCalls = () => {
+//     const res1 = fetch.get('https://api.github.com/users?since=1000') 
+//     const res2 = fetch.get(`https://api.github.com/user/${res1[3]}`) 
+// }
+// const m = ajaxCalls()
+
+// server request 를 보내고, server에서 response 가 온다. 응답시간은 천차만별임.
+
+// res1 에는 request 를 하자마자 바로 결과가 담긴다. 즉 response된 결과가 담기는게 아니라, 불필요한 데이터가 담긴다.
+
+// 결국 위의 방식은 동기처리나 다름없음
+
+/* 
+
+콜백 방식의 비동기처리 ( JQuery )
+
+    $.ajax({
+        method: 'GET',
+        url: 'https://api.gitgub.com',
+        success: function(res) {
+            const res2 = fetch.get(`https://api.github.com/user/${res1[3]}`)
+        }
+    })
+
+*/
+
+/*
+
+Promise 방식의 비동기처리
+
+fetch.get('https://api.github.com/users?since=1000')
+.then(function (res) {
+    const res2 = fetch.get(`https://api.github.com/user/${res1[3]}`)
+})
+
+*/
+
+/* 
+
+Generator 방식의 비동기처리
+
+*/
+
+const fetchWrapper = (gen, url) => fetch(url)
+    .then(res => res.json())
+    .then(res => gen.next(res))
+
+function* getNthUserInfo() {
+    const [gen, from, nth] = yield
+    const req1 = yield fetchWrapper(gen, `https://api.github.com/users?since=${from || 0}`)
+    const userId = req1[nth - 1 || 0].id
+    console.log(userId)
+    const req2 = yield fetchWrapper(gen, `https://api.github.com/user/${userId}`)
+    console.log(req2)
+}
+
+const runGenerator = (generator, ...rest) => {
+    const gen = generator()
+    gen.next()
+    gen.next([gen, ...rest])
+}
+
+runGenerator(getNthUserInfo, 1000, 4)
+runGenerator(getNthUserInfo, 1000, 6)
